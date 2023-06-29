922a76cc7400fcea32fa78a6660373555bc24f7f
Foparty <alfonsomartingarcia@gmail.com>
2023-06-29 08:09:32 +0200

    image section ok with js and conditional for overall theme

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+const body = document.body;
 const rightNav = document.querySelector('.right-nav');
 const leftHeader = document.querySelector('.left-side');
 
+if (
+	window.matchMedia &&
+	window.matchMedia('(prefers-color-scheme: dark)').matches
+) {
+	body.classList = 'dark';
+} else {
+	body.classList = 'light';
+}
 const device = navigator.userAgent;
 
 if (device.includes('iPhone' || 'iPad')) {
