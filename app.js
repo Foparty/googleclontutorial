0b1bfe36b594a7d1530996c5c318be521ca3cf17
Foparty <alfonsomartingarcia@gmail.com>
2023-06-29 10:55:17 +0200

    offered by google line done not real geo location

--- app.js
+++ app.js
@@ -16,3 +16,11 @@ if (device.includes('iPhone' || 'iPad')) {
 	rightNav.style.display = 'none';
 	leftHeader.style.display = 'block';
 }
+
+// Check if the Geolocation API is supported by the browser
+if (navigator.geolocation) {
+	// Call the getCurrentPosition method to get the user's location
+	navigator.geolocation.getCurrentPosition(showPosition);
+} else {
+	console.log('Geolocation is not supported by this browser.');
+}
